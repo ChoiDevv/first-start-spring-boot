ee8d4e2a6636d80f21e5db06617de6c1c29c4531
ChoiDevv <oinkoink71@naver.com>
2022-12-10 21:07:45 +0900

    add: 구글 로그인 세션에 대한 어노테이션을 직접 생성

--- src/main/java/com/practice/web/controller/IndexController.java
+++ src/main/java/com/practice/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.practice.web.controller;
 
+import com.practice.config.auth.LoginUser;
 import com.practice.config.auth.dto.SessionUser;
 import com.practice.web.dto.PostsResponseDto;
 import com.practice.web.service.PostsService;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
